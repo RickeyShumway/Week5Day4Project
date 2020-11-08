fd130eb67f9e52eb94cc97854600207d2448b56c
Rickey Shumway <eric.g.shumway@gmail.com>
2020-11-07 17:49:05 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+$(".navItem").css("color", "white");
+
+$("#nav").css("background-color", "#145");
+
+$("li:even").css("background-color", '#ccc')
+$("li:odd").css("background-color", '#aaa')
+
+$('#myInput').val("Rickey")
+////let listStuff = $("#contentContainer").children()[3];
+//let evenItem = $(listStuff).children()[0];
+//$(evenItem).css("background-color", '#ccc');
+///let oddItem = $(listStuff).children() [1];
+//$(oddItem).css("background-color", '#aaa');
+//l //, 2, 4, 6, 8, 10];
+//console.log(evenItem);
+//let odd = $("listStuff").children() [1] //, 3, 5, 7, 9];
+
+//
+
+//$("odd").css('background-color', '#aaa');
